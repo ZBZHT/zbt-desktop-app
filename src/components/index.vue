d6d4c2627a003ad8eb216abd9843ed095135e796
bwyfed <36172659@qq.com>
2018-11-17 13:09:40 +0800

    提交初始化代码。

---
+++ src/components/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="hello">
+    <el-button type="success">信息按钮</el-button>
+    <video src="http://192.168.0.251:8001/resource/新能源汽车/纯电动汽车/高压安全操作与保养维护/高压安全操作/高压安全操作-11.10.mp4"
+           autoplay controls="controls" controlslist="nodownload"
+    ></video>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'index',
+    data () {
+      return {
+      }
+    },
+    methods: {
+      onPlayerPlay () {
+        alert('play')
+      },
+      onPlayerPause () {
+        alert('pause')
+      }
+    },
+    computed: {
+      player () {
+        return this.$refs.videoPlayer.player
+      }
+    }
+  }
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,28 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import VideoPlayer from 'vue-video-player'
+require('video.js/dist/video-js.css')
+require('vue-video-player/src/custom-theme.css')
+Vue.use(VideoPlayer)
+
+Vue.config.productionTip = false
+
+// for auto update
+import { checkUpdate } from '@/utils/update.js'
+checkUpdate()
+
+Vue.use(ElementUI)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App),
+  template: '<App/>',
+  components: { App }
+})
